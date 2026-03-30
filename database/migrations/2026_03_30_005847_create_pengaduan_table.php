5451e3b7516bc7e747ed00d3bebf6a0cfffa8c23
Rakafikri <rakafikri1929@gmail.com>
2026-03-30 08:03:03 +0700

    Buat database

---
+++ database/migrations/2026_03_30_005847_create_pengaduan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->id();
+            $table->
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('kategori');
+            $table->text('pesan_laporan');
+            $table->enum('status', ['pending', 'proses', 'selesai', 'ditolak'])->default('pending');
+            $table->text('tanggapan_admin')->nullable();
+            $table->foreignId('admin_id')->nullable()->constrained('users'); // admin yang handle
+            $table->timestamp('selesai_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+};
